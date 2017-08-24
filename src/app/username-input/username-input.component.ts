78f98c683aeef74749095af27420bae173668a75
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-08-24 09:19:48 -0400

    assignment 2

--- src/app/username-input/username-input.component.ts
+++ src/app/username-input/username-input.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-username-input',
-  templateUrl: './username-input.component.html',
-  styleUrls: ['./username-input.component.css']
+    selector: 'app-username-input',
+    templateUrl: './username-input.component.html',
+    styleUrls: ['./username-input.component.css']
 })
 export class UsernameInputComponent implements OnInit {
+    username: String = '';
+    constructor() { }
 
-  constructor() { }
+    ngOnInit() {
+    }
 
-  ngOnInit() {
-  }
-
+    usernameIsValid() {
+        return (this.username.length > 0);
+    }
+    onButtonClick() {
+        this.username = '';
+    }
 }
