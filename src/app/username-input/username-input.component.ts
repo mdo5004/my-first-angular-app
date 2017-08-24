91c19d737e82112c32d4e8180055cb715ca81406
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-08-24 09:08:17 -0400

    two-way data binding

---
+++ src/app/username-input/username-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-username-input',
+  templateUrl: './username-input.component.html',
+  styleUrls: ['./username-input.component.css']
+})
+export class UsernameInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
